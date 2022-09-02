da56d9663bb78f52c2d20e8d66fd0ce163b17bb5
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2022-09-02 15:34:30 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useRef, createRef } from 'react';
 import './style.css';
 import ReactAudioPlayer from 'react-audio-player';
 //...
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 export default function App() {
   const audioList1 = [
@@ -83,20 +85,123 @@ export default function App() {
         'https://res.cloudinary.com/ehsanahmadi/video/upload/v1573550770/Sirvan-Khosravi-Dorost-Nemisham-128_kb8urq.mp3',
     },
   ];
+  const musicTracks = [
+    {
+      name: 'Memories',
+      src: 'https://www.bensound.com/bensound-music/bensound-memories.mp3',
+    },
+    {
+      name: 'Creative Minds',
+      src: 'https://www.bensound.com/bensound-music/bensound-creativeminds.mp3',
+    },
+    {
+      name: 'Acoustic Breeze',
+      src: 'https://www.bensound.com/bensound-music/bensound-acousticbreeze.mp3',
+    },
+    {
+      name: 'Sunny',
+      src: 'https://www.bensound.com/bensound-music/bensound-sunny.mp3',
+    },
+    {
+      name: 'Tenderness',
+      src: 'https://www.bensound.com/bensound-music/bensound-tenderness.mp3',
+    },
+    {
+      name: 'Once Again',
+      src: 'https://www.bensound.com/bensound-music/bensound-onceagain.mp3',
+    },
+    {
+      name: 'Sweet',
+      src: 'https://www.bensound.com/bensound-music/bensound-sweet.mp3',
+    },
+    {
+      name: 'Love',
+      src: 'https://www.bensound.com/bensound-music/bensound-love.mp3',
+    },
+    {
+      name: 'Piano Moment',
+      src: 'https://www.bensound.com/bensound-music/bensound-pianomoment.mp3',
+    },
+    {
+      name: 'E.R.F',
+      src: 'https://www.bensound.com/bensound-music/bensound-erf.mp3',
+    },
+    {
+      name: 'Dreams',
+      src: 'https://www.bensound.com/bensound-music/bensound-dreams.mp3',
+    },
+    {
+      name: 'A Day To Remember',
+      src: 'https://www.bensound.com/royalty-free-music/track/a-day-to-remember-wedding-music',
+    },
+    {
+      name: 'Adventure',
+      src: 'https://www.bensound.com/bensound-music/bensound-adventure.mp3',
+    },
+    {
+      name: 'Photo Album',
+      src: 'https://www.bensound.com/bensound-music/bensound-photoalbum.mp3',
+    },
+    {
+      name: 'November',
+      src: 'https://www.bensound.com/bensound-music/bensound-november.mp3',
+    },
+  ];
+
+  const [trackIndex, setTrackIndex] = useState(0);
+
+  const handleClickPrevious = () => {
+    setTrackIndex((currentTrack) =>
+      currentTrack === 0 ? musicTracks.length - 1 : currentTrack - 1
+    );
+  };
+
+  const handleClickNext = () => {
+    setTrackIndex((currentTrack) =>
+      currentTrack < musicTracks.length - 1 ? currentTrack + 1 : 0
+    );
+  };
+  const handleClick = (event) => {
+    const value = parseInt(event.target.id);
+    setTrackIndex(value);
+  };
+  console.log(trackIndex);
   return (
     <div>
-      {audioList1.map((item) => {
-        return (
-          <div>
-            <h1>{item.name}</h1>
-            <h3>By:{item.name}</h3>
-            <div>
-              <img src={item.cover} width={200} height={200} />
+      <div className="playerContainer">
+        {audioList1.map((item, index) => {
+          return (
+            <div key={item.name} className="videoCard">
+              <h1>{item.name}</h1>
+
+              <h3>By:{item.name}</h3>
+              <div onClick={handleClick} id={index}>
+                <img src={item.cover} width={200} height={200} />
+              </div>
+              <button onClick={handleClick} id={index}>
+                Play
+              </button>
             </div>
-            <ReactAudioPlayer src={item.musicSrc} controls />
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      <div className="footer">
+        <AudioPlayer
+          style={{ borderRadius: '1rem' }}
+          // layout="horizontal"
+          autoPlay
+          src={audioList1[trackIndex].musicSrc}
+          onPlay={(e) => console.log('onPlay')}
+          showSkipControls={true}
+          showJumpControls={false}
+          header={`Now playing: ${audioList1[trackIndex].name}`}
+          // footer="All music from: www.bensound.com"
+          onClickPrevious={handleClickPrevious}
+          onClickNext={handleClickNext}
+          onEnded={handleClickNext}
+          // other props here
+        />
+      </div>
     </div>
   );
 }
